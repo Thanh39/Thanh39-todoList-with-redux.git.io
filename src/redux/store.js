7ba7ed4d4ded8c03e98918a36c544d8c252254f3
Thanh39 <72324725+Thanh39@users.noreply.github.com>
2022-07-08 19:03:38 +0700

    Delete src directory

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {createStore} from 'redux'
-import rootReducer from './reducer';
-import {composeWithDevTools} from 'redux-devtools-extension'
-
-const composedEnchancer = composeWithDevTools()
-
-const store =createStore(rootReducer,composedEnchancer)
-
-export default store;
